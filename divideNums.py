32df05c351a0d14b9c4ad01e04207cb54687fd5d
skettzu <99dwong@gmail.com>
2025-02-10 20:13:39 -0500

    latest

--- divideNums.py
+++ divideNums.py
@@ -1,6 +1,8 @@
 def divideTwoNums(num1: int, num2: int) -> float:
+    if (type(num1) or type(num2)) is (str or list):
+        raise TypeError
     try:
-        print(f"Number 1 is {num1} and Number 2 is {num2}")
+        #print(f"Number 1 is {num1} and Number 2 is {num2}")
         result = num1/num2
         return result
     except ZeroDivisionError:
--- test_divideNums.py
+++ test_divideNums.py
@@ -1,23 +1,39 @@
 import unittest
 from divideNums import divideTwoNums
-
+import dataclasses
+'''
 def main():
     test_obj = testDivide()
     test_obj.TestNumDivision()
-    
+'''
 
+@dataclasses
 class testDivide(unittest.TestCase):
-    def TestNumDivision(self):
+    def test_basic_float(self):
         test1 = divideTwoNums(10,2)
-        self.assertEqual(5, test1, "Test1 Failed")
+        self.assertTrue(type(test1) is float, "Test1 Failed")
+
+    def test_decimal(self):
         test2 = divideTwoNums(1,2)
         self.assertEqual(0.5, test2, "Test2 Failed")
+
+    def test_input_decimal(self):
         test3 = divideTwoNums(2.2,1.1)
         self.assertEqual(2, test3, "Test3 Failed")
+
+    def test_large(self):
         test4 = divideTwoNums(-200000000000000000000000000000000,-10000000000000000000000000)
-        print(-200000000000000000000000000000000/-10000000000000000000000000)
         self.assertEqual(20000000, test4, "Test4 Failed")
+
+    def test_zero_div(self):
         self.assertRaises(ZeroDivisionError, divideTwoNums, 2, 0)
+
+    def test_str_input(self):
+        self.assertRaises(TypeError, divideTwoNums, "hello", "world")
+
+    def test_list_input(self):
+        self.assertRaises(TypeError, divideTwoNums, [1,2,3], [4,5,6])
         
 if __name__ == '__main__':
-    main()
+    pass
+    #main()
